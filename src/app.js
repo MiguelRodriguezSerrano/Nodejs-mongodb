b52d067d5d61e76c0c90a3c77880add663ff6c5b
Miguel Angel <mige5553@gmail.com>
2019-07-05 17:02:50 +0100

    Arreglado

--- src/app.js
+++ src/app.js
@@ -1,30 +1,31 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 //Conecting yo DB
-mongoose.connect('mongodb://localhost/crud-mongo')
-    .then(db => console.log('DB connected'))
-    .catch(err => console.log('err'));
+mongoose
+  .connect("mongodb://localhost/")
+  .then(db => console.log("DB connected"))
+  .catch(err => console.log("err"));
 
 //Importing routes
-const indexRoutes = require('./routes/index')
+const indexRoutes = require("./routes/index");
 
 //Settings
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //Middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
-app.use('/', indexRoutes);
+app.use("/", indexRoutes);
 
 //Starting server
-app.listen(app.get('port'), ()=> {
-    console.log(`Server on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,42 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Task = require('../models/task');
+const Task = require("../models/task");
 
-router.get('/', async (req, res) => {
-    const tasks = await Task.find();
-    res.render('index', {
-       tasks
-    });
+router.get("/", async (req, res) => {
+  const task = await Task.find();
+  res.render("index", {
+    tasks: task
+  });
 });
 
-router.post('/add', async (req, res) => {
-    const task = new Task(req.body);
-    await task.save();
-    res.redirect('/');
+router.post("/add", async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.redirect("/");
 });
 
-router.get('/turn/:id', async (req, res) => {
-    const id = req.params;
-    const task = await Task.findById(id);
-    task.status = !task.status;
-    await task.save();
-    res.redirect('/');
-
+router.get("/turn/:id", async (req, res) => {
+  const id = req.params.id;
+  const task = await Task.findById(id);
+  task.status = !task.status;
+  await task.save();
+  res.redirect("/");
 });
 
-router.get('/edit/:id', async (req, res) => {
-    const id = req.params;
-    const task = await Task.findById(id);
-    res.render('edit', {
-        task
-    })
-})
+router.get("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  const task = await Task.findById(id);
+  res.render("edit", {
+    task: task
+  });
+});
 
-router.get('/delete/:id', async (req, res) => {
-    const  id  = req.params;
-    await Task.remove({_id: id});
-    res.redirect('/')
+router.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await Task.remove({ _id: id });
+  res.redirect("/");
 });
 
 module.exports = router;
